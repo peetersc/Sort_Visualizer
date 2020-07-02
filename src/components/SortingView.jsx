ccd22d1bdc36b32873116c8a1d6976d5cae6554e
owoEX <55031938+owoEX@users.noreply.github.com>
2020-07-01 22:49:03 -0700

    Update SortingView.jsx

--- src/components/SortingView.jsx
+++ src/components/SortingView.jsx
@@ -1,53 +1,32 @@
 import React from 'react';
 import './Sorting.css'
+import P5Wrapper from 'react-p5-wrapper'
+import sketch from './sketch'
+import sketch2 from './sketch2'
 
-const NUMBER_OF_ARRAY_BARS = 300;
-
-const PRIMARY_COLOR = 'black';
+import { ThemeContext } from 'styled-components';
 
 export default class SortingVisualizer extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      array: [],
+      stateSketch:sketch,
     };
   }
-
-  componentDidMount() {
-    this.resetArray();
-  }
-
-  resetArray() {
-    const array = [];
-    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
-      array.push(randIntInInterval(10, 500));
-    }
-    this.setState({array});
+  resetArray(){
+    this.state.stateSketch===sketch ? this.setState({stateSketch:sketch2}):this.setState({stateSketch:sketch});
   }
 
    render() {
-    const {array} = this.state;
-
     return (
-      <div className="array-container">
-        {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{
-              backgroundColor: PRIMARY_COLOR,
-              height: `${value}px`,
-            }}></div>
-        ))}
-         <button onClick={() => this.resetArray()}>Generate New Array</button>
+      <div>
+          <P5Wrapper sketch={this.state.stateSketch}></P5Wrapper>
+           <button onClick={() => this.resetArray()}>Generate New Array</button>
       </div>
     );
   }
 }
 
-function randIntInInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
 
