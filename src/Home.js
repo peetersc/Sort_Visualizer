6a959d250a906eb12df785f1761a06d3a89cb4bf
Julian Shalaby <jshalaby@ucsc.edu>
2020-07-03 18:02:48 -0700

    Add files via upload
    updates variables

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
 import SortingView from './components/SortingView';
-
+import sketch from './components/sketch';
+import sketch2 from './components/sketch2';
 const Styles = styled.div`
   .mainbox{
     color: #262626;
@@ -44,35 +45,111 @@ const BeginButton = styled.button`
 `;
 const items = [
   {
-    id: 1,
+    id: 0,
     value: 'Bubble',
   },
   {
-    id: 2,
+    id: 1,
     value: 'Quick',
   },
+  {
+    id: 2,
+    value: 'Merge',
+  },
+  {
+    id: 3,
+    value: 'Heap',
+  },
+  {
+    id: 4,
+    value: 'Selection',
+  }
 ];
+
+function sort(id, sortArray) {
+
+  {/*If id == 0*/}
+  function bubbleSort() {
+      var swapped;
+      do {
+          swapped = false;
+          for (var i=0; i < sortArray.length-1; i++) {
+              if (sortArray[i] > sortArray[i+1]) {
+                  var temp = sortArray[i];
+                  sortArray[i] = sortArray[i+1];
+                  sortArray[i+1] = temp;
+                  swapped = true;
+              }
+          }
+      } while (swapped);
+  }
+
+  {/*If id == 1*/}
+  function quicksort() {
+      if (sortArray.length <= 1) {
+      return sortArray;
+      }
+  
+      var pivot = sortArray[0];
+      
+      var left = []; 
+      var right = [];
+  
+      for (var i = 1; i < sortArray.length; i++) {
+          sortArray[i] < pivot ? left.push(sortArray[i]) : right.push(sortArray[i]);
+      }
+  
+      return quicksort(left).concat(pivot, quicksort(right));
+  };
+
+  {/*If id == 4*/}
+  function selectionSort(){
+      for (let i = 0; i < sortArray.length; i++){
+          let indexOfMin = i // set index of min to the 
+          let j = i // set the index to run the while loop for calculating min
+          let min = sortArray[i] // set the initial value of minimum
+  
+          // find the minimum from i to end of array
+          while (j < sortArray.length){
+              if (sortArray[j] < min){
+                  min = sortArray[j]
+                  indexOfMin = j
+              }
+              j++
+          }
+  
+          if (indexOfMin !== i){ // only swap if the index of minimum and curr item is different
+              let tmp = sortArray[i]
+              sortArray[i] = sortArray[indexOfMin]
+              sortArray[indexOfMin] = tmp
+              console.log(sortArray)
+          }
+      }
+  }
+
+}
 
 function Home(){
     let id = 0;
     return (
-        <div className="container">        
+        <div className="container">  
             <Styles>
               <div>
               <input type="number" placeholder="Size" min="10" max="100"></input>
-                <Button primary onClick={() => console.log("click")}>Bubble</Button>
-                <Button primary>Quick</Button>
-                <Button primary>Merge</Button>
-                <Button primary>Heap</Button>
-                <Button primary>Selection</Button>
-                <BeginButton primary>Begin Sort</BeginButton>
+                <Button primary onClick={() => id=0}>Bubble</Button>
+                <Button primary onClick={() => id=1}>Quick</Button>
+                <Button primary onClick={() => id=2}>Merge</Button>
+                <Button primary onClick={() => id=3}>Heap</Button>
+                <Button primary onClick={() => id=4}>Selection</Button>
+                <BeginButton primary>Begin Sort</BeginButton>  {/*onClick={() => sort(id)}*/}
               </div>
 
                 <div className="mainbox">
-                  <h1>{items[id].value + " Sort"}</h1>
+                  {/*make it update after ID gets changed*/}
+                  <h1>{items[id].value + " Sort"} {console.log(id)}</h1>
                   <div>
                     <SortingView/>
-                  </div>     
+                  </div>
                   <div className="footer">
 
                   </div>
