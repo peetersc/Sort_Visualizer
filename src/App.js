cf5ac511a0b51240314cc9b30bfb74feccf4645a
Atticus Jones <atticusjonesslc@gmail.com>
2020-07-07 18:46:21 -0600

    Removed nav bar. Made rectangles instead of lines

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {About} from './About';
 import {Contact} from './Contact';
 import {NoMatch} from './NoMatch';
 import Layout from './components/Layout';
-import {NavigationBar} from './components/NavigationBar';
 import {Jumbotron} from './components/Jumbotron';
 
 
@@ -15,7 +14,6 @@ class App extends Component{
   render(){
     return(
       <React.Fragment>
-        <NavigationBar />
         <Jumbotron/>
         <Layout>
           <Router>
--- src/components/sketch2.js
+++ src/components/sketch2.js
@@ -4,7 +4,7 @@ export default function sketch (p){
     //FOR EVERY SINGLE p5 FUNCTION U GOTTA do p.FUNCTIONNAME = function (){
     //and to call any p5 function u gotta do p.draw() or p.size()
     
-    const height =300
+    const height = 300
     const width = 800
 
     p.setup =function (){
@@ -18,11 +18,20 @@ export default function sketch (p){
         }
     }
 
+    // Atticus
+    let numBars = 100;
+    let barWidth = width / numBars;
     p.draw =function (){
-        p.background(0);
-        for (let i = 0; i < sortArray.length; i++) {
-            p.stroke(255);
-            p.line(i, height, i, height - sortArray[i]);
+        // Scale and translate are done so that the origin is in the bottom left
+        // instead of the top left, and so that rectangles are drawn from the
+        // bottom up instead of top down.
+        p.scale(1,-1);
+        p.translate(0, -height);
+        // p.background(255);
+        for (let i = 0; i < numBars; i++) {
+            p.stroke(0);
+            p.fill(100);
+            p.rect(i*barWidth, 0, barWidth, sortArray[i]);
         }
     }
 }
