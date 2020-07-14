525d0ddcc72b723b151dd9edb06b544efe6ab876
owoEX <55031938+owoEX@users.noreply.github.com>
2020-07-13 19:25:24 -0700

    fixed mergesort, and some highlighting

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -23,7 +23,7 @@ export default function sortingSketch (p){
         sortArray=new Array(arraySize);
         arrayColor=new Array(arraySize);
         for (let i = 0; i < sortArray.length; i++) {
-            sortArray[i]=p.random(height);
+            sortArray[i]=p.random(height)+10;
             //arrayColor[i]=-1;
             arrayColor[i]='floralwhite';
         }
@@ -42,10 +42,10 @@ export default function sortingSketch (p){
         p.scale(1,-1);
         p.translate(0, -height);
         p.background(38, 38, 38);
-        p.stroke(0);    
+        p.noStroke();    
         if(beginSort.isPressed)
         {
-            resetArray();
+            //resetArray();
             beginSort.isPressed=false;        
         }
         if (beginSort.active){
@@ -78,7 +78,7 @@ export default function sortingSketch (p){
              if(iterator.next().done)
              {
                 for (let i = 0; i < arraySize; i++) {
-                    arrayColor[i]='DarkSeaGreen';
+                    arrayColor[i]='green';
                 }
              }
         }
@@ -125,9 +125,8 @@ export default function sortingSketch (p){
                 arrayColor[j] = 'Maroon ';
                 //do a quick pause to show they swapped, then set the swapped element to a specific color 
                 //and the other element to its original color
-                await sleep(speed*.25)
-                arrayColor[j] = 'indigo ';
-                arrayColor[j+1] = 'floralwhite ';
+                //await sleep(speed*.25)
+                arrayColor[j]= 'floralwhite';
               }
             }
             //set all elements swapped along the way of the inner loop back to their original colors
@@ -147,52 +146,37 @@ export default function sortingSketch (p){
       for (let j = low; j < high; j++) {
 
         //initialize the starying and ending points with the same color
-        arrayColor[low] = 'green';
-        arrayColor[high - 1] = 'green';
-
+        for (let i = 0; i < sortArray.length; i++) {
+          if(arrayColor[i]==='red')
+            arrayColor[i]='floralwhite '
+        }        
+        arrayColor[low] = 'blue';
+        arrayColor[high - 1] = 'blue';
+        arrayColor[piv]='red';
+        arrayColor[j]='red';
+        
         //sleep before begin the iteration
         await sleep(speed*.25)
-
-        //every bar between the pivot and j gets marked as the same color
-        if(j-1>low && arrayColor[j]=='floralwhite '){
-          arrayColor[j-1] = 'indigo ';
-        }
-
-        //mark j as its own color
-        arrayColor[j] = 'maroon ';
-
-        //mark the pivot with its own color
-          arrayColor[piv] = 'blue ';
-
-          //if high-1 gets overriden by j on the last iteration, reset it
-        arrayColor[high - 1] = 'green';
         yield;
-
         if (arr[j] < pivot) {
           //quick pause before the swap
           await sleep(speed*.25)
           swap(arr, piv, j);
-
           //swap the colors to show the swap, then do a quick pause
-          arrayColor[j] = 'blue ';
-          arrayColor[piv] = 'maroon ';
           await sleep(speed*.25)
-
           //swap j back, set the pivot to neutral, and set a new pivot
-          arrayColor[j] = 'maroon ';
-          arrayColor[piv]='floralwhite ';
           piv++
-          arrayColor[piv] = 'blue ';
         }
       }
       swap(arr, piv, high - 1)
+      arrayColor[piv]='green';
     }
     async function* quicksort(arr, low, high) {
     //can go 0 to 1250. 100-speed because speed works with sleeps so its inverted
     let speed=sliderVal;
     speed=100-speed;
     speed*=12.5;
-
+      /*
     //reset everything back to neutral at the top of a recursive call
     for (let i = 0; i < sortArray.length; i++) {
       arrayColor[i]='floralwhite '
@@ -200,7 +184,7 @@ export default function sortingSketch (p){
     //then set all the sorted items to a color
     for (let i = 0; i < low; i++) {
       arrayColor[i]='DarkSeaGreen '
-    }
+    }*/
       if (low < high) {
         yield* partition(arr, low, high, speed)
         yield* quicksort(arr, low, piv)
@@ -272,13 +256,12 @@ export default function sortingSketch (p){
             arrayColor[j] = 'Maroon ';                        //current traversing index
             arrayColor[min_index] = 'blue';               //current min index
 
-            //reset first element to its own color since first iter will override it
-            arrayColor[i] = 'green ';
             //quick pause to show colors
             await sleep(speed*.25)
             yield;
+            arrayColor[j]='floralwhite'
             //trailing colors between min and i set to their own color
-            arrayColor[j] = 'indigo ';                    
+            //arrayColor[j] = 'indigo ';                    
             arrayColor[min_index] = 'floralwhite ';             
 
             if (sortArray[j] < sortArray[min_index]) {    //comparison for smaller value
@@ -289,7 +272,6 @@ export default function sortingSketch (p){
               }
             }
           }
-
           // Swap with current element
           if (min_index !== i) {
             //swap and show changes with color swap
@@ -297,37 +279,33 @@ export default function sortingSketch (p){
             await sleep(speed*.5)
             swap(sortArray, i, min_index);
             arrayColor[i] = 'blue ';
-            arrayColor[min_index] = 'green ';
             await sleep(speed*.25)
           }
           // Current element is correctly sorted
-          arrayColor[i] = 'DarkSeaGreen';
-          yield ;
+          arrayColor[i] = 'green';
         }
     }
 
 
     async function* merge(arr,low,middle,high){
 
-        /*let i = low;
+        let i = low;
         let j = middle + 1;
         while (i <= middle && j <= high) {
-          arrayColor[i] = 'IndianRed ';
-          arrayColor[j] = 'IndianRed ';
+          arrayColor[i] = 'red ';
+          arrayColor[j] = 'red';
           yield;
           arrayColor[i] = 'floralwhite ';
           arrayColor[j] = 'floralwhite ';
           if (arr[i] > arr[j]) {
             for (let k = i; k <= j; ++k) {
               swap(arr, k, j);
-              swap(arr, k, j);
             }
             ++j;
             ++middle;
           }
           ++i;
-        }*/ 
-        //shit doesnt work lol
+        }
     }
     async function* mergeSort(arr,low,high){      
       //can go 0 to 1250. 100-speed because speed works with sleeps so its inverted
@@ -335,11 +313,12 @@ export default function sortingSketch (p){
     speed=100-speed;
     speed*=12.5;
         if(low<high){
-           let middle = ((low+high)/2);
-            yield mergeSort(arr,low,middle)
-            yield mergeSort(arr,middle+1,high);
-            yield merge(arr,low,middle,high);
+           let middle = p.floor((low+high)/2);
+            yield* mergeSort(arr,low,middle)
+            yield* mergeSort(arr,middle+1,high);
+            yield* merge(arr,low,middle,high);
         }
+
     }
 
 }
